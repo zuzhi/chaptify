9f797a273c1ef46aba5cde7dfe9b5734b4891958
zuzhi <zuzhi.hu@gmail.com>
2024-09-05 23:24:46 +0800

    install cljs module

---
+++ src/cljs/zuzhi/chaptify/core.cljs
@@ -0,0 +1,25 @@
+(ns zuzhi.chaptify.core
+  (:require
+    [reagent.core :as r]
+    [reagent.dom :as d]))
+
+
+;; -------------------------
+;; Views
+
+(defn home-page
+  []
+  [:div [:h2 "Welcome to Reagent!"]])
+
+
+;; -------------------------
+;; Initialize app
+
+(defn ^:dev/after-load mount-root
+  []
+  (d/render [home-page] (.getElementById js/document "app")))
+
+
+(defn ^:export ^:dev/once init!
+  []
+  (mount-root))
