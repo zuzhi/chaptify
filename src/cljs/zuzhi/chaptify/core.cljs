b6922d094d7b15ab2720e819f329c5289a213e0a
zuzhi <zuzhi.hu@gmail.com>
2024-09-08 11:06:18 +0800

    integrate instantdb successfully

--- src/cljs/zuzhi/chaptify/core.cljs
+++ src/cljs/zuzhi/chaptify/core.cljs
@@ -1,7 +1,8 @@
 (ns zuzhi.chaptify.core
   (:require
+    ["@instantdb/react" :as instantdb]
     [reagent.core :as r]
-    [reagent.dom :as d]
+    [reagent.dom.client :as rdc]
     [reitit.core :as rt]
     [reitit.frontend.easy :as rt-easy]))
 
@@ -10,6 +11,14 @@
 
 (def projects (r/atom [{:id 1 :name "fullstackopen" :status "normal" :progress 0}]))
 (def archives (r/atom [{:id 2 :name "educated" :status "archived" :progress 100}]))
+
+
+;; ID for app: chaptify
+(def app-id "71eb5a33-d683-4c63-8638-109df239ec0a")
+
+
+;; Initialize the InstantDB
+(def db (instantdb/init #js {:appId app-id}))
 
 
 ;; -------------------------
@@ -48,10 +57,55 @@
        projects-count " active, " archives-count " archived."]])])
 
 
+(defn instant-dashboard
+  []
+  (let [query {:projects {}}
+        result (.useQuery db (clj->js query))
+        {:keys [isLoading error data]} (js->clj result :keywordize-keys true)
+        projects (or (:projects data) [])
+        projects-count (count (filter #(= (:status %) "normal") projects))
+        archives-count (count (filter #(= (:status %) "archived") projects))]
+    (cond
+      isLoading
+      [:div "loading"]
+
+      error
+      [:div (str "Error fetching data: " (.-message error))]
+
+      :else
+      [:p "you have " (+ projects-count archives-count) " projects. "
+       projects-count " active, " archives-count " archived."])))
+
+
+(defn fn-instant-dashboard
+  []
+  [:div
+   [root]
+   [:f> instant-dashboard]])
+
+
 (defn projects-page
   [projects]
   (js/console.log projects)
   [:ul (for [p projects] [:li {:key (:id p)} (:name p)])])
+
+
+(defn instant-projects-page
+  []
+  (let [query {:projects {:$ {:where {:status 'normal}}}}
+        result (.useQuery db (clj->js query))
+        {:keys [isLoading error data]} (js->clj result :keywordize-keys true)
+        projects (or (:projects data) [])]
+    (cond
+      isLoading
+      [:div "loading"]
+
+      error
+      [:div (str "Error fetching data: " (.-message error))]
+
+      :else
+      [:ul
+       (for [p projects] [:li {:key (:id p)} (:name p)])])))
 
 
 (defn archives-page
@@ -60,18 +114,36 @@
   [:ul (for [p archives] [:li {:key (:id p)} (:name p)])])
 
 
+(defn instant-archives-page
+  []
+  (let [query {:projects {:$ {:where {:status 'archived}}}}
+        result (.useQuery db (clj->js query))
+        {:keys [isLoading error data]} (js->clj result :keywordize-keys true)
+        projects (or (:projects data) [])]
+    (cond
+      isLoading
+      [:div "loading"]
+
+      error
+      [:div (str "Error fetching data: " (.-message error))]
+
+      :else
+      [:ul
+       (for [p projects] [:li {:key (:id p)} (:name p)])])))
+
+
 (defn profile
   [{:keys [username tab]}]
   [:div
    [root]
    (cond
-     (= tab "projects") [projects-page @projects]
-     (= tab "archives") [archives-page @archives]
+     (= tab "projects") [:f> instant-projects-page]
+     (= tab "archives") [:f> instant-archives-page]
      :else [:div [:p "hi, " username]])])
 
 
 (def pages
-  {:home #'dashboard
+  {:home #'fn-instant-dashboard
    :profile #'profile})
 
 
@@ -113,9 +185,12 @@
 ;; -------------------------
 ;; Initialize app
 
+(defonce app (rdc/create-root (js/document.getElementById "app")))
+
+
 (defn ^:dev/after-load mount-root
   []
-  (d/render [page] (.getElementById js/document "app")))
+  (.render app (r/as-element [page])))
 
 
 (defn ^:export ^:dev/once init!
